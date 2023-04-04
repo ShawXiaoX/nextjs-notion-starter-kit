0adc343d81542957ba459db6d41b7a1285dbcfd6
ShawXiaoX <59223367+ShawXiaoX@users.noreply.github.com>
2023-04-04 20:13:20 +0800

    Update site.config.ts
    modify basic info to shawxiao

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'shawXiao's blog',
+  domain: 'https://nextjs-notion-starter-kit-five-psi.vercel.app/',
+  author: 'shawXiao',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'ShawXiaox',
+  github: 'ShawXiaoX',
+  linkedin: 'ShawXiao',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
